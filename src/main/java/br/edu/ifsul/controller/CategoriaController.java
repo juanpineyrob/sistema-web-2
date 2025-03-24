73d01001566f0c5d49c2a73979598defde0ac519
Moria Figueroa <correiamoria@gmail.com>
2025-03-24 20:28:13 -0300

    findAll and insert methods

--- src/main/java/br/edu/ifsul/controller/CategoriaController.java
+++ src/main/java/br/edu/ifsul/controller/CategoriaController.java
@@ -1,25 +1,47 @@
 package br.edu.ifsul.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.edu.ifsul.dto.CategoriaDTO;
-import br.edu.ifsul.service.CategoryService;
+import br.edu.ifsul.service.CategoriaService;
 
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaController {
 
     @Autowired
-    CategoryService categoryService;
+    CategoriaService categoriaService;
 
     @GetMapping("/{id}")
     public ResponseEntity<CategoriaDTO> findById(@PathVariable Long id) {
-        CategoriaDTO dto = categoryService.findById(id);
+        CategoriaDTO dto = categoriaService.findById(id);
         return ResponseEntity.ok(dto);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<CategoriaDTO>> findAll(Pageable pageable) {
+        Page<CategoriaDTO> categorias = categoriaService.findAll(pageable);
+        return ResponseEntity.ok(categorias);
+    }
+
+    @GetMapping
+    public ResponseEntity<CategoriaDTO> insert(@RequestBody CategoriaDTO dto) {
+        dto = categoriaService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+            .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+
 }
--- src/main/java/br/edu/ifsul/service/CategoriaService.java
+++ src/main/java/br/edu/ifsul/service/CategoriaService.java
@@ -1,17 +1,21 @@
 package br.edu.ifsul.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.edu.ifsul.dto.CategoriaDTO;
+import br.edu.ifsul.dto.CursoDTO;
 import br.edu.ifsul.entity.Categoria;
 import br.edu.ifsul.repository.CategoriaRepository;
 
 @Service
-public class CategoryService {
+public class CategoriaService {
 
     @Autowired
     CategoriaRepository categoriaRepository;
@@ -20,5 +24,25 @@ public class CategoryService {
     public CategoriaDTO findById(Long id) {
         Optional<Categoria> categoria = categoriaRepository.findById(id);
         return new CategoriaDTO(categoria.get());
+    }
+
+    @Transactional(readOnly = true)
+    public Page<CategoriaDTO> findAll(Pageable pageable) {
+        Page<Categoria> categorias = categoriaRepository.findAll(pageable);
+        return categorias.map(CategoriaDTO::new);   
+    }
+
+    @Transactional
+    public CategoriaDTO insert(CategoriaDTO dto) {
+        Categoria categoria = new Categoria();
+    }
+
+    private void copyDtoToEntity(CategoriaDTO dto, Categoria entity) {
+        entity.setId(dto.getId());
+        entity.setNome(dto.getNome());
+        List<CursoDTO> cursos = dto.getCursos();
+        for(CursoDTO c : cursos) {
+            entity.getCursos().add(cursos);
+        }
     }
 }
